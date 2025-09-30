395dc7402660d3cdc96300c588425e8ab65ec1fd
zsline <sergei.dereka@gmail.com>
2025-09-30 09:04:35 +0300

    first commit

---
+++ themes/gamestore/patterns/hidden-404.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Title: 404
+ * Slug: gamestore/404
+ * Inserter: no
+ *
+ * @package gamestore
+ * @since 1.0.0
+ */
+?>
+<!-- wp:heading {"textAlign":"center", "level": 1} -->
+<h1 class="wp-block-heading has-text-align-center">
+	<?php esc_html_e( 'Page not found', 'gamestore' ); ?>
+</h1>
+<!-- /wp:heading -->
+<!-- wp:paragraph -->
+<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try a search?', 'gamestore' ); ?></p>
+<!-- /wp:paragraph -->
+<!-- wp:search {"label":"<?php echo esc_html_x( 'Search', 'Search form label', 'gamestore' ); ?>","showLabel":false,"buttonText":"<?php echo esc_html_x( 'Search', 'Search form submit button text', 'gamestore' ); ?>"} /-->
